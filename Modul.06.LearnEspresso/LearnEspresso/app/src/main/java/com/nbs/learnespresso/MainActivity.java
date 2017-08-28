3bafe5409fb59cf2444c849e2cee946ef5e2f7cd
ahmadimaduddin <olele76@gmail.com>
2017-08-28 15:01:11 +0700

    add LearnEspresso

---
+++ Modul.06.LearnEspresso/LearnEspresso/app/src/main/java/com/nbs/learnespresso/MainActivity.java
@@ -0,0 +1,51 @@
+package com.nbs.learnespresso;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener{
+
+    private EditText edtText;
+    private Button btnChangeText, btnSwitchActivity;
+    private TextView tvPrintedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Learn Espresso");
+
+        edtText = (EditText)findViewById(R.id.edt_text);
+        btnChangeText = (Button)findViewById(R.id.btn_change_text);
+        btnSwitchActivity = (Button)findViewById(R.id.btn_swicth_activity);
+        tvPrintedText = (TextView)findViewById(R.id.tv_printed_text);
+
+        btnChangeText.setOnClickListener(this);
+        btnSwitchActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_change_text:
+                edtText.setText("Lalala");
+                String input = edtText.getText().toString().trim();
+                tvPrintedText.setText(input);
+                break;
+
+            case R.id.btn_swicth_activity:
+                String text = edtText.getText().toString().trim();
+                Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra(SecondActivity.EXTRA_INPUT, text);
+                startActivity(intent);
+                break;
+        }
+    }
+}
---
+++ Modul.06.LearnEspresso/LearnEspresso/app/src/main/java/com/nbs/learnespresso/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.nbs.learnespresso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    private TextView tvResultView;
+    public static String EXTRA_INPUT = "extra_input";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        getSupportActionBar().setTitle("Activity Second");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tvResultView = (TextView)findViewById(R.id.tv_result_view);
+        String input = getIntent().getStringExtra(EXTRA_INPUT);
+        tvResultView.setText(input);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
