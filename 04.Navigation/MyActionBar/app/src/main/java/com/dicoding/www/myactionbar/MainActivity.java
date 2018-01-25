73862905fe41253551b45a2848d25ca024259e3d
ahmadimaduddin <olele76@gmail.com>
2018-01-25 14:03:23 +0700

    Optimize layout

--- 04.Navigation/MyActionBar/app/src/main/java/com/dicoding/www/myactionbar/MainActivity.java
+++ 04.Navigation/MyActionBar/app/src/main/java/com/dicoding/www/myactionbar/MainActivity.java
@@ -1,11 +1,11 @@
 package com.dicoding.www.myactionbar;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
--- 04.Navigation/MyActionBar/app/src/main/java/com/dicoding/www/myactionbar/MenuActivity.java
+++ 04.Navigation/MyActionBar/app/src/main/java/com/dicoding/www/myactionbar/MenuActivity.java
@@ -1,15 +1,8 @@
 package com.dicoding.www.myactionbar;
 
 
-import android.content.Intent;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
