ba9ae8ec9c963bfc4d1fd3a8e38cea6e4e1f6b27
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2018-09-05 13:27:52 +0700

    remove redundant code & add non-null annotation

--- 04.Navigation/MyNavigationDrawer/app/src/main/java/com/dicoding/mynavigationdrawer/PageFragment.java
+++ 04.Navigation/MyNavigationDrawer/app/src/main/java/com/dicoding/mynavigationdrawer/PageFragment.java
@@ -2,6 +2,7 @@ package com.dicoding.mynavigationdrawer;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -27,12 +28,12 @@ public class PageFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_halaman, container, false);
 
-        TextView textView = (TextView) view.findViewById(R.id.tv_fragment);
+        TextView textView = view.findViewById(R.id.tv_fragment);
 
         /*
         Ambil data dari arguments yang dikirimkan oleh activity
