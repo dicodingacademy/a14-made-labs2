337c6643455fd4fe86a3c3039f91c6d774f50555
Arif <faizin.arief@gmail.com>
2019-08-02 16:02:59 +0700

    View group, style, custom view, and recyclerview

---
+++ 01.Views and group/MyConstraintView/kotlin/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.dicoding.picodiploma.myrecyclerview
+
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.picodiploma.myrecyclerview.adapter.CardViewHeroAdapter
+import com.dicoding.picodiploma.myrecyclerview.adapter.GridHeroAdapter
+import com.dicoding.picodiploma.myrecyclerview.adapter.ListHeroAdapter
+import com.dicoding.picodiploma.myrecyclerview.model.Hero
+import com.dicoding.picodiploma.myrecyclerview.model.HeroesData
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private var title = "Mode List"
+    private lateinit var rvHeroes: RecyclerView
+    private val list = ArrayList<Hero>()
+    private var mode: Int = 0
+
+    companion object {
+        private const val STATE_TITLE = "state_string"
+        private const val STATE_LIST = "state_list"
+        private const val STATE_MODE = "state_mode"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+        /*
+        Gunakanlah savedinstancestate untuk menjaga data ketika terjadi config changes
+         */
+        when (savedInstanceState) {
+            null -> {
+                /*
+            Pada saat pertama kali activity dijalankan,
+            Ambil data dari method getListData, kemudian tampilkan recyclerviewlist
+             */
+                setActionBarTitle(title)
+                list.addAll(HeroesData.listData)
+                showRecyclerList()
+                mode = R.id.action_list
+
+            }
+            else -> {
+                /*
+            Jika terjadi config changes maka ambil data yang dikirimkan dari saveinstancestate
+             */
+                title = savedInstanceState.getString(STATE_TITLE).toString()
+                val stateList = savedInstanceState.getParcelableArrayList<Hero>(STATE_LIST)
+                val stateMode = savedInstanceState.getInt(STATE_MODE)
+
+                /*
+            Set data untuk title, list, dan mode yang dipilih
+             */
+                setActionBarTitle(title)
+                if (stateList != null) {
+                    list.addAll(stateList)
+                }
+                setMode(stateMode)
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_TITLE, title)
+        outState.putParcelableArrayList(STATE_LIST, list)
+        outState.putInt(STATE_MODE, mode)
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
+
+        listHeroAdapter.setOnItemClickCallback(object : ListHeroAdapter.OnItemClickCallback{
+            override fun onItemClicked(data: Hero) {
+                showSelectedHero(data)
+            }
+        })
+    }
+
+    private fun showRecyclerGrid() {
+        rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+
+        gridHeroAdapter.setOnItemClickCallback(object : GridHeroAdapter.OnItemClickCallback{
+            override fun onItemClicked(data: Hero) {
+                showSelectedHero(data)
+            }
+        })
+    }
+
+    private fun showRecyclerCardView() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val cardViewHeroAdapter = CardViewHeroAdapter(list)
+        rvHeroes.adapter = cardViewHeroAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                title = "Mode List"
+                showRecyclerList()
+            }
+
+            R.id.action_grid -> {
+                title = "Mode Grid"
+                showRecyclerGrid()
+            }
+
+            R.id.action_cardview -> {
+                title = "Mode CardView"
+                showRecyclerCardView()
+            }
+        }
+
+        mode = selectedMode
+        setActionBarTitle(title)
+    }
+
+    private fun setActionBarTitle(title: String?) {
+            supportActionBar?.title = title
+    }
+
+    private fun showSelectedHero(hero: Hero) {
+        Toast.makeText(this, "Kamu memilih ${hero.name}", Toast.LENGTH_SHORT).show()
+    }
+
+
+}
