39592c22dc50c968a4f3405f70d56d58ff979171
Arif <faizin.arief@gmail.com>
2019-10-31 10:52:19 +0700

    Remove unused import

--- Navigation/MyNavigationDrawer/java/app/src/main/java/com/dicoding/picodiploma/mynavigationdrawer/MainActivity.java
+++ Navigation/MyNavigationDrawer/java/app/src/main/java/com/dicoding/picodiploma/mynavigationdrawer/MainActivity.java
@@ -1,18 +1,14 @@
 package com.dicoding.picodiploma.mynavigationdrawer;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
