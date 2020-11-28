cc561010790313116153e10e563e855399b04e6c
Dimas Catur <dimascatur48@gmail.com>
2020-11-28 12:30:39 +0700

    Update to Android Studio 4.1

--- Navigation/MyNavigationDrawer/java/app/src/main/java/com/dicoding/picodiploma/mynavigationdrawer/ui/home/HomeFragment.java
+++ Navigation/MyNavigationDrawer/java/app/src/main/java/com/dicoding/picodiploma/mynavigationdrawer/ui/home/HomeFragment.java
@@ -6,11 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.dicoding.picodiploma.mynavigationdrawer.R;
 
@@ -21,7 +21,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
--- Navigation/MyNavigationDrawer/java/app/src/main/java/com/dicoding/picodiploma/mynavigationdrawer/ui/share/ShareFragment.java
+++ Navigation/MyNavigationDrawer/java/app/src/main/java/com/dicoding/picodiploma/mynavigationdrawer/ui/share/ShareFragment.java
@@ -6,12 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.dicoding.picodiploma.mynavigationdrawer.R;
 
