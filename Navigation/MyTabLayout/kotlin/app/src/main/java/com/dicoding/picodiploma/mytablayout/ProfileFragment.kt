0b8cefe4b5f5dba4715cddc59c0aff3fa3ccbfb4
Dimas Catur <dimascatur48@gmail.com>
2020-12-01 08:24:25 +0700

    Update to Android Studio 4.1

--- Navigation/MyTabLayout/kotlin/app/src/main/java/com/dicoding/picodiploma/mytablayout/ProfileFragment.kt
+++ Navigation/MyTabLayout/kotlin/app/src/main/java/com/dicoding/picodiploma/mytablayout/ProfileFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dicoding.picodiploma.mytablayout.databinding.FragmentProfileBinding
 
 
 /**
@@ -15,11 +16,19 @@ import android.view.ViewGroup
  */
 class ProfileFragment : Fragment() {
 
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
