d82a9b429523bf68769dd1937a358b8e760f1a69
Dimas Catur <dimascatur48@gmail.com>
2020-12-01 22:20:10 +0700

    Update to Android Studio 4.1

--- Navigation/MyNavigation/kotlin/app/src/main/java/com/dicoding/picodiploma/mynavigation/CategoryFragment.kt
+++ Navigation/MyNavigation/kotlin/app/src/main/java/com/dicoding/picodiploma/mynavigation/CategoryFragment.kt
@@ -7,12 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_category.*
+import com.dicoding.picodiploma.mynavigation.databinding.FragmentCategoryBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class CategoryFragment : Fragment() {
+
+    private var _binding: FragmentCategoryBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         val EXTRA_NAME = "extra_name"
@@ -22,24 +25,31 @@ class CategoryFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        btn_category_lifestyle.setOnClickListener { view ->
+//       binding.btnCategoryLifestyle.setOnClickListener { view ->
 //            val mBundle = Bundle()
 //            mBundle.putString(EXTRA_NAME, "Lifestyle")
 //            mBundle.putLong(EXTRA_STOCK, 7)
 //            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
 //        }
 
-        btn_category_lifestyle.setOnClickListener { view ->
+        binding.btnCategoryLifestyle.setOnClickListener { view ->
             val toDetailCategoryFragment = CategoryFragmentDirections.actionCategoryFragmentToDetailCategoryFragment()
             toDetailCategoryFragment.name = "Lifestyle"
             toDetailCategoryFragment.stock = 7
             view.findNavController().navigate(toDetailCategoryFragment)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- Navigation/MyNavigation/kotlin/app/src/main/java/com/dicoding/picodiploma/mynavigation/HomeFragment.kt
+++ Navigation/MyNavigation/kotlin/app/src/main/java/com/dicoding/picodiploma/mynavigation/HomeFragment.kt
@@ -7,29 +7,37 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_home.*
+import com.dicoding.picodiploma.mynavigation.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btn_category.setOnClickListener(
+        binding.btnCategory.setOnClickListener(
                 Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_categoryFragment)
         )
-        btn_profile.setOnClickListener { view ->
+        binding.btnProfile.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_homeFragment_to_profileActivity)
         }
-
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
