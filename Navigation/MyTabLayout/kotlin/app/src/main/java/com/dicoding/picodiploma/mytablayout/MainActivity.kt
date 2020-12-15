f7d95126ed308bd225b59f519b53b00b5ea7f575
Arif <faizin.arief@gmail.com>
2020-12-15 16:17:59 +0700

    Upgrade to ViewPager2

--- Navigation/MyTabLayout/kotlin/app/src/main/java/com/dicoding/picodiploma/mytablayout/MainActivity.kt
+++ Navigation/MyTabLayout/kotlin/app/src/main/java/com/dicoding/picodiploma/mytablayout/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.dicoding.picodiploma.mytablayout
 
 import android.os.Bundle
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    @StringRes
+    private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2,
+            R.string.tab_text_3)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
 
         supportActionBar?.elevation = 0f
     }
--- Navigation/MyTabLayout/kotlin/app/src/main/java/com/dicoding/picodiploma/mytablayout/SectionsPagerAdapter.kt
+++ Navigation/MyTabLayout/kotlin/app/src/main/java/com/dicoding/picodiploma/mytablayout/SectionsPagerAdapter.kt
@@ -1,20 +1,12 @@
 package com.dicoding.picodiploma.mytablayout
 
-import android.content.Context
-import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
 
-    @StringRes
-    private val TAB_TITLES = intArrayOf(
-            R.string.tab_text_1,
-            R.string.tab_text_2,
-            R.string.tab_text_3)
-
-//    override fun getItem(position: Int): Fragment {
+//    override fun createFragment(position: Int): Fragment {
 //        var fragment: Fragment? = null
 //        when (position) {
 //            0 -> fragment = HomeFragment()
@@ -23,15 +15,11 @@ class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) :
 //        return fragment as Fragment
 //    }
 
-    override fun getItem(position: Int): Fragment {
+    override fun createFragment(position: Int): Fragment {
         return HomeFragment.newInstance(position + 1)
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return mContext.resources.getString(TAB_TITLES[position])
-    }
-
-    override fun getCount(): Int {
+    override fun getItemCount(): Int {
         return 3
     }
 }
