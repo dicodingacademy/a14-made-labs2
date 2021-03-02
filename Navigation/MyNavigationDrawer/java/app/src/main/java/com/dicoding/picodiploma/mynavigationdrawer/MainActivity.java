9828469f9d1ee39742be9279c6774bbe3748c207
Arif <faizin.arief@gmail.com>
2021-03-02 12:56:42 +0700

    Update Android Studio 4.1.2

--- Navigation/MyNavigationDrawer/java/app/src/main/java/com/dicoding/picodiploma/mynavigationdrawer/MainActivity.java
+++ Navigation/MyNavigationDrawer/java/app/src/main/java/com/dicoding/picodiploma/mynavigationdrawer/MainActivity.java
@@ -57,8 +57,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_cart)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_cart)
                 .setOpenableLayout(drawerLayout)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
